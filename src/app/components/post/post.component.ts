cd0c2a83350fea45cc1ed752c17c06ee000e2abc
Omar <os.omarsalah@gmail.com>
2025-01-20 17:16:16 +0200

    enhance post components with comment count display and limit comments shown; implement post fetching in post component

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  PostResponseModel,
+  PostService,
+} from '../../services/post/post.service';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  post!: PostResponseModel;
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      const postId = params.get('postId');
+      if (postId) {
+        this.fetchPostDetails(+postId);
+      } else {
+        this.toastr.error('Invalid Post ID', 'Error');
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  fetchPostDetails(id: number) {
+    this.postService.getPostById(id).subscribe({
+      next: (post) => {
+        this.post = post;
+        console.log('Fetched user post:', post);
+      },
+      error: (error) => {
+        this.toastr.error(
+          'Failed to fetch post. Please try again later.',
+          'Error'
+        );
+        this.router.navigate(['/not-found']);
+        console.error('Error fetching user post:', error);
+      },
+    });
+  }
 }
